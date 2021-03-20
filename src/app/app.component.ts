917aa058a55287d30b154462e7cfd8bcc026d7d0
Matthew Kempey <mkempey@gmail.com>
2021-03-20 09:20:36 -0400

    added initial skills component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'matthewkempey';
+  public title: string = 'matthewkempey';
 }
